429cbc2fd086a813993b2ce22abeb7606ee99737
Shaun M <shaun-mccabe@outlook.com>
2024-05-13 05:43:11 -0400

    got everything but the likes done

--- forms.py
+++ forms.py
@@ -6,20 +6,30 @@ from wtforms.validators import DataRequired, Email, Length
 class MessageForm(FlaskForm):
     """Form for adding/editing messages."""
 
-    text = TextAreaField('text', validators=[DataRequired()])
+    text = TextAreaField("text", validators=[DataRequired()])
 
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[Length(min=6)])
-    image_url = StringField('(Optional) Image URL')
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+    image_url = StringField("(Optional) Image URL")
 
 
 class LoginForm(FlaskForm):
     """Login form."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+
+
+class UserEditForm(FlaskForm):
+    """Form For Editing the user profile"""
+
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    bio = TextAreaField("Bio")
+    location = StringField("location")
+    image_url = StringField("Profile Image")
+    header_image_url = StringField("Header Image URL")
